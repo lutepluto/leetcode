e597dff5e685e99069a9c46c76db2053f91cc555
lutepluto <lutepluto@gmail.com>
2019-01-28 20:01:02 +0800

    problem 88: merge sorted array

---
+++ problem88/merge_sorted_array_test.go
@@ -0,0 +1,14 @@
+package problem88
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMerge(t *testing.T) {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+
+	merge(nums1, 3, nums2, 3)
+	fmt.Printf("After merge: %#v\n", nums1)
+}
